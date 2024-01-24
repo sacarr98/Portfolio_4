a65d8ec104f4eeb25ad0296829b5e89f563e7ba1
sacarr98 <>
2024-01-24 10:34:31 +0000

    create comments model

--- news/models.py
+++ news/models.py
@@ -13,3 +13,15 @@ class News(models.Model):
     image = models.ImageField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    news = models.ForeignKey(
+        News, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
