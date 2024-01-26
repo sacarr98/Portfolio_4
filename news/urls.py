6ba94d3080dcce3d4b21c51ddb47ab3b4a92d2c9
sacarr98 <>
2024-01-26 11:12:16 +0000

    create news_detail view and html structure to view individual news stories

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.NewsList.as_view(), name='home'),
+    path('<slug:slug>/', views.news_detail, name='news_detail')
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import News
 
@@ -9,3 +9,21 @@ class NewsList(generic.ListView):
     queryset = News.objects.all()
     template_name = "news/index.html"
     pagniate_by = 6
+
+
+def news_detail(request, slug):
+    """
+    Display an individual :model:`news.News`.
+
+    **Context**
+
+    ``news``
+        An instance of :model:`news.News`.
+
+    **Template:**
+
+    :template:`news/news_detail.html`
+    """
+    queryset = News.objects.filter(status=1)
+    news = get_object_or_404(queryset, slug=slug)
+    return render(request, "news/news_detail.html", {"news": news},)
