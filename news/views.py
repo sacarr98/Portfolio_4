e8403e7de8d8ad9f457e8e959d2c03e9299f0567
sacarr98 <>
2024-01-29 06:26:15 +0000

    add crispy forms to allow users to leave a comment

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import News
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -26,4 +27,22 @@ def news_detail(request, slug):
     """
     queryset = News.objects.filter(status=1)
     news = get_object_or_404(queryset, slug=slug)
-    return render(request, "news/news_detail.html", {"news": news},)
+    comments = news.comments.all().order_by("-created_on")
+    comment_count = news.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.news = news
+            comment.save()
+
+    comment_form = CommentForm()
+
+    return render(request, "news/news_detail.html", {
+        "news": news, "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+    },
+    )
