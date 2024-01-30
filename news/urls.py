8ad43163cea1dc30d2246ea51bda69a338a94de3
sacarr98 <>
2024-01-30 11:33:01 +0000

    add functionality to edit and delete comments

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.NewsList.as_view(), name='home'),
     path('<slug:slug>/', views.news_detail, name='news_detail')
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
